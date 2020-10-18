a75c901428c45e6c57a7da823c891f44b04e637a
toshi <toshi@toshi.click>
2020-10-18 15:32:38 +0900

    Django REST frameworkチュートリアル その４.２ スニペットを登録するときに自動でリクエストを送った人がownerとなるように登録

--- backend/containers/django/restapi/views.py
+++ backend/containers/django/restapi/views.py
@@ -8,6 +8,8 @@ class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
